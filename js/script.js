4bc74fe8d634a0af09ad13fb3bb7d0cc447f74da
madang11 <madang@s.sfusd.edu>
2019-10-08 01:08:52 +0000

    piglatin

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $( "document" ).ready(function() {
+    $("#button").click(function(){
+        let input = $(".input").val();
+        let vowels = ["a","e","i","o","u"]
+        if (vowels.includes(input.charAt(0)){
+            let first = input.charAt(0);
+            let second = input.substring(1);
+            let final = second + first + "ay";
+            $("p2").text("final");
+    }
+    else {
 
 
 
+
+        $("p2").text("hi");
+         }
+    });
 });
